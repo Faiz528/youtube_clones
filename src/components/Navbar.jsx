544d5b20531335c4fc0018305a587e912feecef2
Md Faiz Salahuddin <mdfaizsalahuddin@gmail.com>
2025-07-03 09:03:57 +0000

    SearchBar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Stack  } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { logo } from '../utils/constants'
+import SearchBar from './SearchBar'
 
 
 
@@ -9,10 +10,11 @@ const Navbar = () => {
   return (
     <div>Navbar
       <Stack direction="row" alignItems="center" p={2}
-        sx={{ position: 'sticky', top: 0, background: '#000', padding: '10px' }}>
+        sx={{ position: 'sticky', top: 0, background: '#000', padding: '10px', justifyContent:"space-between" }}>
          <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
           <img src={logo} alt="logo" height={45}/>
           </Link>
+          <SearchBar/>
     </Stack>
     </div>
   )
